0108aee05b7bb995de0cdaf9e89874e2cd201eb7
KD <kandarpgupta97@gmail.com>
2019-02-26 22:42:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/kd/blackspot/OTP_Verification.java
@@ -0,0 +1,111 @@
+package com.example.kd.blackspot;
+
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class OTP_Verification extends AppCompatActivity {
+
+    String phone_number, verificationId;
+    private EditText editText;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp__verification);
+
+        phone_number = getIntent().getStringExtra("number");
+
+        editText = findViewById(R.id.otp);
+        mAuth = FirebaseAuth.getInstance();
+        sendVerificationCode(phone_number);
+        findViewById(R.id.signin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String code = editText.getText().toString().trim();
+                if(code.isEmpty() || code.length()<6)
+                {
+                    editText.setError("Enter code....");
+                    editText.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+            }
+        });
+    }
+
+    private void verifyCode(String code)
+    {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredentials(credential);
+    }
+
+    private void signInWithCredentials(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(), "You Are Successful", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "This is an error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sendVerificationCode(String number) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number, 60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks
+        );
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null)
+            {
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getApplicationContext(), "Exception is :" + e, Toast.LENGTH_SHORT).show();
+        }
+    };
+}
---
+++ app/src/main/java/com/example/kd/blackspot/Signin.java
@@ -0,0 +1,35 @@
+package com.example.kd.blackspot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Signin extends AppCompatActivity {
+
+    EditText p_number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        p_number= findViewById(R.id.phone_number);
+        findViewById(R.id.done).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = p_number.getText().toString().trim();
+                if(num.isEmpty() || num.length()<10)
+                {
+                    Toast.makeText(getApplicationContext(), "Incorrect", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(Signin.this, OTP_Verification.class);
+                intent.putExtra("number", num);
+                startActivity(intent);
+            }
+        });
+    }
+}
