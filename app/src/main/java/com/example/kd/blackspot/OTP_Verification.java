d3dfd5bdc6d548d689a41c76185cf6d752a4983d
KD <kandarpgupta97@gmail.com>
2019-03-04 22:33:24 +0530

    UI changes

--- app/src/main/java/com/example/kd/blackspot/OTP_Verification.java
+++ app/src/main/java/com/example/kd/blackspot/OTP_Verification.java
@@ -1,7 +1,6 @@
 package com.example.kd.blackspot;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
